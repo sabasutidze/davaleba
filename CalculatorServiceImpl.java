4c6d45b5479f38581eb32fc8c2efdbe3684055cf
sabasutidze <72156377+sabasutidze@users.noreply.github.com>
2020-09-30 21:00:32 +0400

    Add files via upload

---
+++ CalculatorServiceImpl.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class CalculatorServiceImpl {
+
+    public static void main(String[] args){
+        int x = 60;
+        int y = 20;
+        System.out.println("the sum is " + Integer.sum(x,y));
+    }
+}
